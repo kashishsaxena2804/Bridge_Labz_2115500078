aca8f9accc92bb427e2a238376a908c86dbd2c2c
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-01-22 14:26:59 +0530

    Level-1 Ques3 Km to Miles

---
+++ KmtoMiles.cs
@@ -0,0 +1,10 @@
+using System;
+class KmtoMiles{
+	public static void Main(string[] args){
+		double Km = 10.8;
+		
+		double miles= Km*1.6;
+		
+		Console.WriteLine("The distance " + Km + " Km in miles is " + miles);
+	}
+}
