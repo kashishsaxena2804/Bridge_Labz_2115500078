2a565aab3405bcaa11d52664c6e6a09ae927a4d7
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-01-25 18:46:38 +0530

    Level1 Ques2 Check Numbers (Positive, Negative, Zero) and Compare First and Last

---
+++ CheckNumbers.cs
@@ -0,0 +1,32 @@
+using System;
+
+class CheckNumbers
+{
+    // public static void Main()
+    // {
+        // int[] numbers = new int[5];
+
+        // for (int i = 0; i < 5; i++)
+        // {
+            // Console.Write($"Enter number {i + 1}: ");
+            // numbers[i] = int.Parse(Console.ReadLine());
+        // }
+
+        // foreach (int num in numbers)
+        // {
+            // if (num > 0)
+                // Console.WriteLine($"{num} is positive and {(num % 2 == 0 ? "even" : "odd")}.");
+            // else if (num < 0)
+                // Console.WriteLine($"{num} is negative.");
+            // else
+                // Console.WriteLine("0 is zero.");
+        // }
+
+        // if (numbers[0] > numbers[^1])
+            // Console.WriteLine("The first number is greater than the last.");
+        // else if (numbers[0] < numbers[^1])
+            // Console.WriteLine("The first number is less than the last.");
+        // else
+            // Console.WriteLine("The first number is equal to the last.");
+    // }
+}
