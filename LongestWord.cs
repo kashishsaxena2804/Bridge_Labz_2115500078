1f2b6ee012bc469d7f14660f8513f5e7741f0665
Kashish Saxena <kashish.saxena1306@gmail.com>
2025-01-29 22:32:19 +0530

    String Handling Ques5 Find the Longest Word in a Sentence

---
+++ LongestWord.cs
@@ -0,0 +1,24 @@
+using System;
+
+class LongestWord
+{
+    static string LongestWord(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        string longest = "";
+
+        foreach (string word in words)
+        {
+            if (word.Length > longest.Length)
+                longest = word;
+        }
+        return longest;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string input = Console.ReadLine();
+        Console.WriteLine("Longest Word: " + LongestWord(input));
+    }
+}
